f142c892582674a05c3e53d7379cd12ad466b4f3
25eqsg3f08 <25eqsg3f08@g.elctp.k12.edu.mo>
2026-02-05 17:03:15 +0800

    Update blocked repository name in JAM_CONFIG

--- jam.js
+++ jam.js
@@ -2,7 +2,7 @@
 export const JAM_CONFIG = {
     // 手动添加要屏蔽的仓库名称（支持多个，直接写仓库名即可）
     blockedRepoNames: [
-        '25eqsg3f08-stack.github.io',
+        '25eqsg3f08.github.io',
   // 主索引仓库（必屏蔽）
         // 可在此处添加其他需要屏蔽的仓库，示例：
         // 'test-repo',
